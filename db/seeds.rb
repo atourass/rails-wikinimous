6369a4225e81310cbce5a2845c0d0bb6516a6950
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-11-21 17:02:37 +0100

    1st optional done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'kramdown'
+
+Article.destroy_all
+
+puts 'start...'
+10.times {
+  article = Article.create!(
+    title: Kramdown::Document.new(Faker::Book.title).to_html,
+    content: Kramdown::Document.new(Faker::Movie.quote).to_html
+  )
+}
+
+puts "10 articles created."
+
+
+
